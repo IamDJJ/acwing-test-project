0f3c0275674a0ee03a20b95dd2d03a3d622dcd6a
IamDjj <609060079@qq.com>
2020-03-22 23:23:19 +0800

    AcWing算法题，A + B问题，简单的函数问题。

---
+++ src/easy/aAndBProblem.java
@@ -0,0 +1,15 @@
+package easy;
+
+class aAndBProblem {
+    public static int getCount(int a, int b) {
+        if(a <= 0 || b >= Math.pow(10, 8)) {
+            //log: a和b的取值范围不对
+            return 0;
+        }
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getCount(6,100000000 - 1));
+    }
+}
